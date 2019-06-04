dd18780909a85830c7e9bd404f13e507dfa96027
Kane Cheshire <kane.cheshire@googlemail.com>
2019-06-05 00:01:39 +0100

    Deleted old files

--- Old/Example/Pods/Target Support Files/Pods-PixelTest_Example-PixelTestExampleSnapshotTests/Pods-PixelTest_Example-PixelTestExampleSnapshotTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PixelTest_Example_PixelTestExampleSnapshotTests : NSObject
-@end
-@implementation PodsDummy_Pods_PixelTest_Example_PixelTestExampleSnapshotTests
-@end
